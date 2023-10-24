14ec5b96e60a249998273223177e11a759e4f466
Cory Charlton <ccharlton@pobox.com>
2023-10-24 10:23:58 -0700

    Adding IHostBuilder extensions

---
+++ CCSWE.nanoFramework.Mediator/ConfigureAsyncMediatorOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace CCSWE.nanoFramework.Mediator
+{
+    /// <summary>
+    /// An action for configuring the <see cref="AsyncMediator"/>.
+    /// </summary>
+    /// <param name="options"></param>
+    public delegate void ConfigureAsyncMediatorOptions(AsyncMediatorOptions options);
+}
